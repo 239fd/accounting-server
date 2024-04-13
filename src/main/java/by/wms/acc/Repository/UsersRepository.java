d72fc49f4f1cb4ed9edb09c67ecc740cffba7b04
Pavel Karliuk <126984519+239fd@users.noreply.github.com>
2024-04-13 18:49:29 +0300

    Update UsersRepository.java

--- src/main/java/by/wms/acc/Repository/UsersRepository.java
+++ src/main/java/by/wms/acc/Repository/UsersRepository.java
@@ -1,2 +1,9 @@
-package by.wms.acc.Repository;public interface UsersRepository {
+package by.wms.acc.Repository;
+
+import by.wms.acc.Entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Integer> {
 }
