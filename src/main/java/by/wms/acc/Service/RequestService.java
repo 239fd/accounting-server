e59fb0f591c8385c5de1f22ee668b92c59b8d2d0
Pavel Karliuk <126984519+239fd@users.noreply.github.com>
2024-04-13 18:49:54 +0300

    Update RequestService.java

--- src/main/java/by/wms/acc/Service/RequestService.java
+++ src/main/java/by/wms/acc/Service/RequestService.java
@@ -1,2 +1,31 @@
-package by.wms.acc.Service;public class RequstService {
+package by.wms.acc.Service;
+
+import by.wms.acc.DTO.RequestDTO;
+import by.wms.acc.Entity.Request;
+import by.wms.acc.Repository.RequestRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class RequestService {
+
+    private final RequestRepository requestRepository;
+
+    public Request create(RequestDTO dto){
+        Request request = Request.builder()
+                .status(dto.getStatus())
+                .date(dto.getDate())
+                .build();
+        return requestRepository.save(request);
+    }
+
+    public List<Request> readAll(){
+        return requestRepository.findAll();
+    }
+
+
+
 }
