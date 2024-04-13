5e5dc3bd6ba03a4b022c0a84057b84212e60d3a9
Pavel Karliuk <126984519+239fd@users.noreply.github.com>
2024-04-13 18:47:43 +0300

    Update GoodsDTO.java

--- src/main/java/by/wms/acc/DTO/GoodsDTO.java
+++ src/main/java/by/wms/acc/DTO/GoodsDTO.java
@@ -1,2 +1,15 @@
-package by.wms.acc.DTO;public class GoodsDTO {
+package by.wms.acc.DTO;
+
+import lombok.Data;
+
+@Data
+public class GoodsDTO {
+
+    private String name;
+    private Integer amount;
+    private Float length;
+    private Float width;
+    private Float height;
+    private Float weight;
+
 }
