710ae6e365df7b3dbc68259619788cc6c7f4114b
Pavel Karliuk <126984519+239fd@users.noreply.github.com>
2024-04-13 18:48:35 +0300

    Update Request.java

--- src/main/java/by/wms/acc/Entity/Request.java
+++ src/main/java/by/wms/acc/Entity/Request.java
@@ -1,2 +1,29 @@
-package by.wms.acc.Entity;public class Request {
+package by.wms.acc.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "request")
+public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer ID;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "date")
+    private Date date;
+
 }
