881b0fd69de3981c73af43bb2e3a19278ccaead4
Pavel Karliuk <126984519+239fd@users.noreply.github.com>
2024-04-13 18:48:48 +0300

    Update Users.java

--- src/main/java/by/wms/acc/Entity/Users.java
+++ src/main/java/by/wms/acc/Entity/Users.java
@@ -1,2 +1,34 @@
-package by.wms.acc.Entity;public class Users {
+package by.wms.acc.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer ID;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "apostryc")
+    private String apostryc;
 }
