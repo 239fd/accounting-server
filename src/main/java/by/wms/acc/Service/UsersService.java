66751b92a8277f14e9da49d8df0fce6765a23960
Pavel Karliuk <126984519+239fd@users.noreply.github.com>
2024-04-13 18:50:06 +0300

    Update UsersService.java

--- src/main/java/by/wms/acc/Service/UsersService.java
+++ src/main/java/by/wms/acc/Service/UsersService.java
@@ -1,2 +1,30 @@
-package by.wms.acc.Service;public class UsersService {
+package by.wms.acc.Service;
+
+import by.wms.acc.DTO.UsersDTO;
+import by.wms.acc.Entity.Users;
+import by.wms.acc.Repository.UsersRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public Users create(UsersDTO dto){
+        Users users = Users.builder()
+                .login(dto.getLogin())
+                .password(dto.getPassword())
+                .name(dto.getName())
+                .surname(dto.getSurname())
+                .apostryc(dto.getApostryc())
+                .build();
+        return usersRepository.save(users);
+    }
+
+    public Users update(Users users){
+        return usersRepository.save(users);
+    }
+
 }
