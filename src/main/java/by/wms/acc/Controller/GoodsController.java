bd7c4e24db929d1770779143d71b82c045bae122
Pavel Karliuk <126984519+239fd@users.noreply.github.com>
2024-04-13 18:46:53 +0300

    Update GoodsController.java

--- src/main/java/by/wms/acc/Controller/GoodsController.java
+++ src/main/java/by/wms/acc/Controller/GoodsController.java
@@ -1,2 +1,40 @@
-package by.wms.acc.Controller;public class GoodsController {
+package by.wms.acc.Controller;
+
+import by.wms.acc.DTO.GoodsDTO;
+import by.wms.acc.Entity.Goods;
+import by.wms.acc.Service.GoodsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/goods")
+@AllArgsConstructor
+public class GoodsController {
+
+    private final GoodsService goodsService;
+
+    @PostMapping
+    public ResponseEntity<Goods> create(@RequestBody GoodsDTO dto){
+        return new ResponseEntity<>(goodsService.create(dto), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Goods> update(@RequestBody Goods goods) {
+        return mappingResponseGoods(goodsService.update(goods));
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpStatus delete(@PathVariable Integer id) {
+        goodsService.delete(id);
+        return HttpStatus.OK;
+    }
+
+    private ResponseEntity<Goods> mappingResponseGoods(Goods goods) {
+        return new ResponseEntity<>(goods, HttpStatus.OK);
+    }
+
+
 }
