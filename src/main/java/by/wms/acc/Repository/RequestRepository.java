e6b5c50b2883c60c0ea5d66ddf0fa4685a8fabf6
Pavel Karliuk <126984519+239fd@users.noreply.github.com>
2024-04-13 18:49:18 +0300

    Update RequestRepository.java

--- src/main/java/by/wms/acc/Repository/RequestRepository.java
+++ src/main/java/by/wms/acc/Repository/RequestRepository.java
@@ -1,2 +1,9 @@
-package by.wms.acc.Repository;public interface RequestRepository {
+package by.wms.acc.Repository;
+
+import by.wms.acc.Entity.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Integer> {
 }
